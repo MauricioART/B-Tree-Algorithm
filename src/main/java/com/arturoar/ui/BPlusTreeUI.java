23132bfb7158189908576fb3a06ae0358a147f97
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2025-02-26 17:17:41 -0600

    Add template UI

--- src/main/java/com/arturoar/ui/BPlusTreeUI.java
+++ src/main/java/com/arturoar/ui/BPlusTreeUI.java
@@ -2,19 +2,53 @@
 package com.arturoar.ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.Group;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+
+
 
 public class BPlusTreeUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(BPlusTreeUI.class.getResource("vista.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+       // FXMLLoader fxmlLoader = new FXMLLoader(BPlusTreeUI.class.getResource("vista.fxml"));
+       // Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         primaryStage.setTitle("B+ Tree");
-        primaryStage.setScene(scene);
+       // primaryStage.setScene(scene);
         primaryStage.setResizable(false);
+       // primaryStage.show();
+       Group root = new Group();
+       Scene scene = new Scene(root,900,600);
+
+       final Canvas canvas = new Canvas(250,250);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        
+        gc.setStroke(Color.BLACK);
+        gc.setFill(Color.AQUAMARINE);
+        gc.fillRect(75,75,100,100);
+        gc.setFont(new Font(STYLESHEET_CASPIAN, 20));
+        gc.fillText("Hola jnijwednoni4fn", 70, 75);
+        Line linea = new Line();
+        linea.setStartX(0.0f);
+        linea.setStartY(0.0f);
+        linea.setEndX(100.0f);
+        linea.setEndY(300.0f);
+        gc.strokeLine(0,0,300,100);  
+        Text texto = new Text(10, 50, "Hola mundo");
+        texto.getLayoutBounds()
+        
+        root.getChildren().add(canvas);
+       
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
