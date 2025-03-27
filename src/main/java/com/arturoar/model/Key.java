b9c3df9482d2625094e6807943001bbc71583b38
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2025-03-27 17:27:21 -0600

    Add Key class to be able to map each key on the tree model to a BPlusKeyView on the view.

---
+++ src/main/java/com/arturoar/model/Key.java
@@ -0,0 +1,38 @@
+package com.arturoar.model;
+
+/**
+ * Represents a key in the B+ Tree.
+ *
+ * @param <K> the type of the key, which must be comparable
+ */
+public class Key<K extends Comparable<K>> {
+
+    /**
+     * The key value.
+     */
+    public K key;
+
+    /**
+     * Constructs a new Key with the specified value.
+     *
+     * @param key the value of the key
+     */
+    Key(K key){
+        this.key = key;
+    }
+
+    /**
+     * Creates a clone of this key with the specified value.
+     *
+     * @param key the value of the new key
+     * @return a new Key object with the specified value
+     */
+    public Key<K> clone(K key){
+        return new Key<K>(key);
+    }
+    
+    public K getKey(){
+        return this.key;
+    }
+    
+}
