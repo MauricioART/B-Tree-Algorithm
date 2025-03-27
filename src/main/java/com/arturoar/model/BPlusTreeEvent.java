a69ede0471f38d75a3c3c34485a7082de78d410f
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2025-03-27 17:26:37 -0600

    Add  BPlusTreeEvent  class to the model to update changes made to the model on the view.

--- src/main/java/com/arturoar/model/BPlusTreeEvent.java
+++ src/main/java/com/arturoar/model/BPlusTreeEvent.java
@@ -5,16 +5,27 @@ public class BPlusTreeEvent<K extends Comparable<K>,V> {
 
     private final EventType type;
     private final BPlusNode<K,V> affectedNode;
-    private final K affectedKey;
+    private final BPlusNode<K,V> affectedNode2;
+    private final Key<K> affectedKey;
 
-    public BPlusTreeEvent(EventType type, BPlusNode<K,V> affectedNode, K affectedKey) {
+    public BPlusTreeEvent(EventType type, BPlusNode<K,V> affectedNode, Key<K> affectedKey) {
         this.type = type;
         this.affectedNode = affectedNode;
         this.affectedKey = affectedKey;
+        this.affectedNode2 = null;
     }
+
+    public BPlusTreeEvent(EventType type, BPlusNode<K,V> lenderNode, BPlusNode<K,V> borrowerNode, Key<K> affectedKey) {
+        this.type = type;
+        this.affectedNode = lenderNode;
+        this.affectedKey = affectedKey;
+        this.affectedNode2 = borrowerNode;
+    }
+    
 
 
     public EventType getType() { return type; }
     public BPlusNode<K,V> getAffectedNode() { return affectedNode; }
-    public K getAffectedKey() { return affectedKey; }
+    public Key<K> getAffectedKey() { return affectedKey; }
+    public BPlusNode<K,V> getAffectedNode2() { return affectedNode2; }
 }
