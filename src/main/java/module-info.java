480409a26b03e2ce732e222ab64eafeda47737cc
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2025-05-12 14:44:38 -0600

    Add transitive keyword for other modules dependeing on that package.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.arturoar.bplustree {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
+    requires transitive javafx.graphics;
     requires java.xml;
     requires javafx.base;
 
